b8a69832fbbda79952163dcdad0e06708fc5627f
IMAD-GN <ibenomar536@gmail.com>
2023-07-10 20:07:17 +0200

    chaine of responsibility pattern

---
+++ Behavioral/State/DoneState.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Behavioral\State;
+
+class DoneState extends State
+{
+    protected string $state = StateEnum::DONE_STATE;
+
+    public function proceed()
+    {
+        // Do nothing
+        $this->transitionTo(new ArchivedState());
+    }
+
+}
---
+++ Behavioral/Strategy/StrategyInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Behavioral\Strategy;
+
+interface StrategyInterface
+{
+    public function encrypt(string $data): array;
+
+}
